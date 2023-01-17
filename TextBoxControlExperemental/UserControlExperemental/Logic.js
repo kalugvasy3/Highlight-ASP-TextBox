24d1cea7cfc7b236b2470074e8c5084b8b81382e
Vasily Kalugin <58394907+kalugvasy3@users.noreply.github.com>
2023-01-17 08:38:19 -0600

    Replace logic

--- TextBoxControlExperemental/UserControlExperemental/Logic.js
+++ TextBoxControlExperemental/UserControlExperemental/Logic.js
@@ -1,12 +1,13 @@
 ﻿
 function txtUp(self) {
 
-    var strtxt = self.value.replace(/\t/g, '    '); // all TAB replace with 4 spaces.
-
-    if (self.clientHeight <= 20) { strtxt.replace(/(\r\n|\n|\r|\n\r)/gm, " ") };
+    //replaceSpecialChars(self);
+    //RemoveSpecialChar(self);
+    var strtxt = self.value;
+    if (self.clientHeight <= 25) { strtxt.replace(/(\r\n|\n|\r|\n\r)/gm, " ") };
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
-    strtxt = strtxt.normalize('NFKC');
+    strtxt = strtxt.normalize('NFKC'); "NFC"
 
     var div = self.parentElement.childNodes[1]; // It will be "divCloneTxt". 
     div.innerHTML = "";
@@ -19,14 +20,15 @@ function txtUp(self) {
 
     for (i = 0; i < origin.length; i++) {
         ch = origin[i];
-        if (ch == ">" || ch == "<" || ch == "&" || ch == "/") // control characters
+        if (isCharNotValid(ch)) //wrong characters
         {
-            ch = " "
-        }
+            var ch1 = ch;
+            if (ch == ">" || ch == "<" || ch == "&" || ch == "/") // control characters
+            {
+                ch1 = "*"
+            }
 
-        if (ch == "!") //wrong characters
-        {
-            strdiv += '<span class="yellowbackground">' + ch + '</span>';
+            strdiv += '<span class="yellowbackground">' + ch1 + '</span>';
             error = true;
         }
         else {
@@ -66,10 +68,101 @@ function txtPaste(self) {
     div.style.fontFamily = self.style.fontFamily;
     div.style.fontSize = self.style.fontSize;
 
-    setTimeout(function () {
-        txtUp(self);
-    }, 100);
+    txtUp(self);
+    txtOnScroll(self);
+
     return true;
 }
+
+function RemoveSpecialChar(self) {
+    if (self.value != '' && self.value.match(/^[\w ]+$/) == null) {
+        self.value = self.value.replace(/[\W]/g, '');
+    }
+}
+
+    function replaceSpecialChars(self) {
+
+        var str = self.value
+
+        str = str.replace('&', ' and ');
+        str = str.replace('<', ' less than ');
+        str = str.replace('>', ' greater than ');
+
+        var regex = /â|ȁ|ä|à|á|ã|å|ă|ꜳ/g;  // "/g" meanse replace all char from list left, do not stop after replacing first char...see below link...
+        str = str.replace(regex, 'a');
+
+        regex = /Â|Ä|À|Á|Ã|Å|Ă/g;
+        str = str.replace(regex, 'A');
+
+        regex = /é|ê|ë|è|ȅ/g;
+        str = str.replace(regex, 'e');
+
+        regex = /ĉ/g;
+        str = str.replace(regex, 'c');
+
+        regex = /É|Ê|Ë|È|Ẽ/g;
+        str = str.replace(regex, 'E');
+
+        regex = /í|î|ï|ì|ἴ|ἶ|ἳ/g;
+        str = str.replace(regex, 'i');
+
+        regex = /Í|Î|Ï|Ì/g;
+        str = str.replace(regex, 'I');
+
+        regex = /ô|ö|ò|ó|õ/g;
+        str = str.replace(regex, 'o');
+
+        regex = /Ô|Ö|Ò|Ó|Õ/g;
+        str = str.replace(regex, 'O');
+
+        regex = /û|ü|ù|ú/g;
+        str = str.replace(regex, 'u');
+
+        regex = /Û|Ü|Ù|Ú/g;
+        str = str.replace(regex, 'U');
+
+        regex = /ÿ|ý|ӱ/g;
+        str = str.replace(regex, 'y');
+
+        regex = /Ÿ|Ý/g;
+        str = str.replace(regex, 'Y');
+
+        regex = /ñ|ῆ/g;
+        str = str.replace(regex, 'n');
+
+        regex = /Ñ|Ṅ|Ӥ/g;
+        str = str.replace(regex, 'N');
+
+        regex = /€|₡|ç|ß|¬|Ç|¦|ø|Ø|æ|Æ|®|£|¥|©|™|¨|ꬿ|ꟹ|℅/g;
+        str = str.replace(regex, ' ');
+
+        str = str.replace('^', ''); // It must be empty.
+
+        regex = /¢|\u005C/g;  // ¢ \ 
+        str = str.replace(regex, ' ');
+
+        regex = /"|'|´|‘|’|“|”|\u0091|\u0092|\u0093|\u0094|\u005C/g;
+        str = str.replace(regex, '`');
+
+
+        str = str.replace('¼', ' 1/4 ');
+        str = str.replace('½', ' 1/2 ');
+        str = str.replace('⅓', ' 1/3 ');
+        str = str.replace('⅔', ' 2/3 ');
+        str = str.replace('¾', ' 3/4 ');
+        str = str.replace('�', ' ');
+        str = str.replace('♀', ' ');
+        str = str.replace('–', '-');
+        str = str.replace(/\t/g, '    '); // all TAB replace with 4 spaces.
+        self.value = str.normalize("NFC");
+    }
+
+    function isCharNotValid(ch) {
+
+        var regex = /[^a-z^A-Z^0-9\^_ ~!@#$?%&*()+-=\{\}\[\]\\|`>\n\s]/g;
+        var chOut = ch.replace(regex, '*');
+        var blnOut = ch != chOut;
+        return blnOut;
+    }
 
 
